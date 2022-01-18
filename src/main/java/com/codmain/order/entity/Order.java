f8693a9d13d38094977b7e479d21c2a6709b03dc
Alexis Petrovich <alexispetrovich11@gmail.com>
2022-01-18 17:40:50 -0300

    add entity Order

---
+++ src/main/java/com/codmain/order/entity/Order.java
@@ -0,0 +1,31 @@
+package com.codmain.order.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "REG_DATE", nullable = false, updatable = false)
+    private LocalDateTime regDate; //fecha de registro
+
+    @OneToMany
+    private List<OrderLine> lines; //caract del producto
+
+    @Column(name = "TOTAL", nullable = false)
+    private Double total; //sumatoria
+
+}
